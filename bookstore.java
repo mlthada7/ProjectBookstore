2fc389208231b2bfb44d7779922cb13d24298a44
Muhammad Multahada <m.multahada@gmail.com>
2020-01-16 09:49:26 +0700

    first commit

---
+++ bookstore.java
@@ -0,0 +1,24 @@
+public class Bookstore {
+	public static void main(String[] args) {
+		double harga1=50000;
+    	int jml=1;
+
+			System.out.println("==========TOKO BUKU JAYA==========");
+			System.out.println("Judul        : Dillan 1990");
+	        System.out.println("Penulis      : Pidi Baiq");
+	        System.out.println("Penerbit     : Pastel Books");
+	        System.out.println("Tahun Terbit : 2014");
+	        System.out.println("Halaman      : 333");
+	        System.out.println("Jumlah       : 1");
+	        System.out.println("Harga        : "+harga1);
+	        System.out.println("Total Harga  : "+(harga1*jml));
+	        System.out.println("------------------------------------------");
+	        System.out.println("Konfirmasi? y/n: y");
+	        System.out.println("----------STRUK PEMBELIAN----------");
+	        System.out.println("________________________________________");
+	        System.out.println("|Dillan 1990                | Rp40.000");
+	        System.out.println("|Total                      | Rp40.000");
+	        System.out.println("________________________________________");
+	        System.out.println("----------TERIMA KASIH----------");
+	   }     
+}
